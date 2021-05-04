0664bb5122adc2e249d611867ebdaf1222fdf883
AArifaarggh <25915021+arifazan@users.noreply.github.com>
2021-05-04 22:15:29 +0900

    Update app.js
    playing around with a simplified declaration

--- src/App.js
+++ src/App.js
@@ -10,8 +10,20 @@ function compare(user){
   return helloWorld;
 }
 
+const simplifiedElements = (
+  'h1',
+  {className: 'simplifiedElements'},
+  'this is a simple element, not that you can see it'
+);
+
 const checkValue = (
   <h1>{compare(user)}</h1>
+);
+
+const divOpener = (
+  'div',
+  {className: 'opener'},
+  <h2>{simplifiedElements}</h2>
 );
 
 function App() {
@@ -22,6 +34,7 @@ function App() {
       </header>
       <body>
           {checkValue}
+          {divOpener}
       </body>
     </div>
   );
